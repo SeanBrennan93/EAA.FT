6b016b914fee176e97251ddd730f4f900258b5ce
SeanBrennan93 <seanbrenn93@gmail.com>
2014-10-10 11:46:33 +0100

    Made some changes

---
+++ week4/data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Please Enter Your name: "
+read input_variable1
+echo "Please Enter the month you were born: "
+read input_variable2
+echo "Please Enter Your favourite color: "
+read input_variable3
+
+echo "Hello $input_variable1 you were born in $input_variable2 and your favourite color is $input_variable3"
---
+++ week4/diary.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title : Question 6
+#: Date : 10/10/14
+#: Author : Sean Brennan
+#: Version : 1
+#: Description : Read Input
+#: Options : n/a
+
+
